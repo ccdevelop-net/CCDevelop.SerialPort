ae51e50ee97c66a1915a8e57b22606e9b8d17c3f
Cristian <cristian.croci@gmail.com>
2024-10-04 13:22:54 +0200

    Update compiler

--- obj/Debug/net8.0/CCDevelop.SerialPort.AssemblyInfo.cs
+++ obj/Debug/net8.0/CCDevelop.SerialPort.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CCDevelop.SerialPort")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b962f4bc46510d6fa7fc990b4189a0826c89c57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de75bd6ad7a4a04f6e42f324b459b5725d633efa")]
 [assembly: System.Reflection.AssemblyProductAttribute("CCDevelop.SerialPort")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CCDevelop.SerialPort")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
